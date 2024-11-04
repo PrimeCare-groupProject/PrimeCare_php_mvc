7711b446736006cd6baddf5555792e1451009cd1
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2024-11-04 11:47:07 +0530

    Update agent.sidebar.php

--- app/views/agent/agent.sidebar.php
+++ app/views/agent/agent.sidebar.php
@@ -1,8 +1,8 @@
 <ul>
-    <li><button class="btn">Dashboard</button></li>
-    <li><button class="btn">Profile</button></li>
-    <li><button class="btn">Pre Inspection</button></li>
-    <li><button class="btn">Requested Tasks</button></li>
-    <li><button class="btn">Task Management</button></li>
-    <li><button class="btn">Manage Booking</button></li>
+    <li><a href="<?= ROOT ?>/dashboard"><button class="btn">Dashboard</button></a></li>
+    <li><a href="<?= ROOT ?>/profile"><button class="btn">Profile</button></a></li>
+    <li><a href="<?= ROOT ?>/preInspection"><button class="btn">Pre Inspection</button></a></li>
+    <li><a href="<?= ROOT ?>/requestedTasks"><button class="btn">Requested Tasks</button></a></li>
+    <li><a href="<?= ROOT ?>/taskManagement"><button class="btn">Task Management</button></a></li>
+    <li><a href="<?= ROOT ?>/manageBooking"><button class="btn">Manage Booking</button></a></li>
 </ul>
