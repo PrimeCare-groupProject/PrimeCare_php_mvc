99fd34d332a50849151bc7e236c4a7f7410b91bc
BimsaraImash <bimsa2021@gmail.com>
2024-11-11 13:54:37 +0530

    Create preTnspection page

---
+++ app/views/components/preInspectionCard.php
@@ -0,0 +1,39 @@
+<div class="preInspection">
+    <div class="preInspection-header">
+        <h3>Property 1</h3>
+        <button class="primary-btn">Accept</button>
+    </div>
+    <div class="input-group1">
+        <div class="input-group2">
+            <div class="input-group">
+                <div class="input-group-aligned">
+                    <span class="input-label-aligend"><strong>Property ID:</strong></span><span class="input-field-small">P2022</span>
+                </div>
+                <div class="input-group-aligned">
+                    <span class="input-label-aligend"><strong>City:</strong></span><span class="input-field-small">2024/08/23</span>
+                </div>
+            </div>
+            <div class="input-group">
+                <div class="input-group-aligned">
+                    <span class="input-label-aligend"><strong>Property Type:</strong></span><span class="input-field-small">House</span>
+                </div>
+                <div class="input-group-aligned">
+                    <span class="input-label-aligend"><strong>Date:</strong></span><span class="input-field-small">2024/08/23</span>
+                </div>
+            </div>
+            <div class="input-group">
+                <div class="input-group-aligned">
+                    <span class="input-label-aligend"><strong>Address:</strong></span><span class="input-field-small">No 90 , Colombo , Sri Lanka</span>
+                </div>
+            </div>
+            <div class="input-group">
+                <div class="input-group-aligned">
+                    <span class="input-label-aligend"><strong>Description:</strong></span><span class="input-field-small">Anything about the property assigning and other details</span>
+                </div>
+            </div>
+        </div>
+        <div>
+            <img class="image-card" src="<?= ROOT ?>/assets/images/resort.png" alt="">
+        </div>
+    </div>
+</div>
