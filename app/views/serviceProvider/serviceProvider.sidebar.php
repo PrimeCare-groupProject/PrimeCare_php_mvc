e1747ca82160cedfc8565a124e975ad538b25c7f
Nimnapathum <pathumeshan123@gmail.com>
2024-11-16 04:09:27 +0530

    delete side bar files

--- app/views/serviceProvider/serviceProvider.sidebar.php
+++
@@ -1,6 +0,0 @@
-<ul>
-    <li><button class="btn">Dashboard</button></li>
-    <li><button class="btn">Profile</button></li>
-    <li><button class="btn">Services</button></li>
-    <li><button class="btn">Logs</button></li>
-</ul>
