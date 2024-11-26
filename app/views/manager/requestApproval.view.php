272935ea885e70201574e7ff141cc5b83136c6aa
Nimnapathum <pathumeshan123@gmail.com>
2024-11-27 03:20:17 +0530

    Refactor: request approval view for manager

--- app/views/manager/requestApproval.view.php
+++ app/views/manager/requestApproval.view.php
@@ -7,12 +7,11 @@
     <h2>request Approval</h2>
     <div class="flex-bar">
         <form class="search-container" method="GET">
-            <input 
-                type="text" 
-                class="search-input" 
-                name="searchterm" 
-                placeholder="find request ..."
-            >
+            <input
+                type="text"
+                class="search-input"
+                name="searchterm"
+                placeholder="Find request...">
             <button class="search-btn" type="submit">
                 <img src="<?= ROOT ?>/assets/images/search.png" alt="Search Icon" class="small-icons">
             </button>
@@ -21,18 +20,18 @@
 </div>
 <div>
     <div class="filter-container-flex-start">
-        <div class="input-group-aligned width-100">
+        <div class="input-group">
             <label for="year-filter" class="input-label">Year:</label>
-            <select id="year-filter" class="input-field-small">
+            <select id="year-filter" class="input-field">
                 <option value="all">All</option>
                 <option value="2024">2024</option>
                 <option value="2023">2023</option>
                 <!-- Add more years as needed -->
             </select>
         </div>
-        <div class="input-group-aligned width-100">
+        <div class="input-group">
             <label for="month-filter" class="input-label">Month:</label>
-            <select id="month-filter" class="input-field-small">
+            <select id="month-filter" class="input-field">
                 <option value="all">All</option>
                 <option value="January">January</option>
                 <option value="February">February</option>
@@ -50,10 +49,44 @@
         </div>
     </div>
     <div>
-        <?php require __DIR__ . '/../components/requestApprovalCard.php'; ?>
-        <?php require __DIR__ . '/../components/requestApprovalCard.php'; ?>
-        <?php require __DIR__ . '/../components/requestApprovalCard.php'; ?>
-        <?php require __DIR__ . '/../components/requestApprovalCard.php'; ?>
+        <?php for ($i = 0; $i < 5; $i++): ?>
+            <div class="approval-container">
+                <div class="approval-left-content">
+                    <h3>Approval Request 1</h3>
+                    <div class="input-group">
+                        <div class="input-group">
+                            <span class="input-label fixed_width_for_label"><strong>Property ID:</strong></span><span class="input-field small_margin">P1236</span>
+                        </div>
+                        <div class="input-group">
+                            <span class="input-label fixed_width_for_label"><strong>Price(LKR):</strong></span><span class="input-field small_margin">200000</span>
+                        </div>
+                    </div>
+                    <div class="input-group">
+                        <div class="input-group">
+                            <span class="input-label fixed_width_for_label"><strong>Date:</strong></span><span class="input-field small_margin">2024/08/23</span>
+                        </div>
+                        <div class="input-group">
+                            <span class="input-label fixed_width_for_label"><strong>Agent ID:</strong></span><span class="input-field small_margin">A3546</span>
+                        </div>
+                    </div>
+                    <div class="input-group">
+                        <div class="input-group">
+                            <span class="input-label fixed_width_for_label"><strong>Address:</strong></span><span class="input-field small_margin">No 90 , Colombo , Sri Lanka</span>
+                        </div>
+                    </div>
+                    <div class="input-group">
+                        <div class="input-group">
+                            <span class="input-label fixed_width_for_label"><strong>Description:</strong></span><span class="input-field small_margin">Anything about the property assigning and other details</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="approval-right-content">
+                    <button class="primary-btn">Accept</button>
+                    <button class="secondary-btn">Decline</button>
+                    <img src="<?= ROOT ?>/assets/images/listing_alt.jpg" alt="property" class="approval-right-content-img">
+                </div>
+            </div>
+        <?php endfor; ?>
     </div>
 </div>
 
