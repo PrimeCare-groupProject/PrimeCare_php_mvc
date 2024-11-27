10358bf4393ee1227454ac96e26b562427d5e172
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-11-27 12:39:29 +0530

    Refactor: implement user view switch functionality and update dashboard routing

--- app/views/customer/customerHeader.view.php
+++ app/views/customer/customerHeader.view.php
@@ -27,17 +27,28 @@
         <div class="header-line">
             <a href="<?= ROOT ?>/home"><img src="<?= ROOT ?>/assets/images/logo.png" alt="PrimeCare" class="header-logo-png"></a>
             <button class="toggle-sidebar-btn" onclick="toggleSidebar()">☰ Menu</button>
-            <a href="<?= ROOT ?>/dashboard/profile"><img src="<?= get_img($_SESSION['user']->image_url) ?>" alt="Profile" class="header-profile-picture"></a>
+            <!-- toggle button -->
+            <form method="post" class="toggle_wrapper" action="<?= ROOT ?>/dashboard/switchUser">
+                <div class="toggle-button tooltip-container">
+                    <span class="tooltip-text">Change To Original Mood</span>
+                    <!-- Outer track -->
+                    <div class="toggle-track activeToggle" id="toggleTrack" onclick="submitToggleForm()">
+                        <!-- Inner knob -->
+                        <div class="toggle-knob"></div>
+                        <input type="hidden" name="toggle_state" id="toggleState" value="<?= isset($_SESSION['toggle_state']) ? $_SESSION['toggle_state'] : '0' ?>">
+                        <input type="submit" name="toggle_btn" value="1" hidden>
+                    </div>
+                </div>
+                <a href="<?= ROOT ?>/dashboard/profile"><img src="<?= get_img($_SESSION['user']->image_url) ?>" alt="Profile Picture" class="header-profile-picture"></a>
+            </form>
         </div>
         <div class="content-section">
             <div class="user_view-sidemenu">
                 <ul>
-                    <li><a href="<?= ROOT ?>/dashboard/search/property/propertyLisingToAllUsers" data-section="propertyLisingToAllUsers"><button class="btn">Search</button></a></li>
-                    <li><a href="<?= ROOT ?>/dashboard/payments" data-section="payments"><button class="btn">Payments</button></a></li>
-                    <li><a href="<?= ROOT ?>/dashboard/occupiedProperties" data-section="occupiedProperties"><button class="btn">Occupied Properties</button></a></li>
-                    <li><a href="<?= ROOT ?>/dashboard/repairListing" data-section="repairListing"><button class="btn">Services</button></a></li>
+                    <li><a href="<?= ROOT ?>/dashboard/search/property/propertyLisingToAllUsers"><button class="btn">Search</button></a></li>
+                    <li><a href="<?= ROOT ?>/dashboard/payments"><button class="btn">Payments</button></a></li>
+                    <li><a href="<?= ROOT ?>/dashboard/occupiedProperties"><button class="btn">Occupied Properties</button></a></li>
                     <li><a href="<?= ROOT ?>/dashboard/profile" data-section="profile"><button class="btn">Profile</button></a></li>
-                    <li><a href="<?= ROOT ?>/dashboard/updateRole" data-section="updateRole"><button class="btn">Update Role</button></a></li>
                 </ul>
                 <form method="post" id="logout">
                     <button id="logout-btn" class="secondary-btn" style="display: none;">Logout</button>
@@ -98,6 +109,20 @@
                     // logoutBtn.addEventListener('click', function() {
                     //     window.location.href = '<?= ROOT ?>/dashboard/logout';
                     // });
+                    //togle logic
+                    const toggleState = document.getElementById('toggleState').value;
+                    const toggleTrack = document.getElementById('toggleTrack');
+                    if (toggleState == true) {
+                        toggleTrack.classList.add('activeToggle');
+                    }
+
+                    toggleTrack.addEventListener('click', () => {
+                        toggleTrack.classList.toggle('activeToggle');
+                        document.getElementById('toggleState').value = toggleTrack.classList.contains('activeToggle') ? '1' : '0';
+                    });
                 });
+                function submitToggleForm() {
+                    document.querySelector('.toggle_wrapper').submit();
+                }
             </script>
             <div class="user_view-content_section" id="content-section">
