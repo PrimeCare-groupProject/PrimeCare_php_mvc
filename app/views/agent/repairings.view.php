a8241045c9f423017b187cb01b6713df498ac27e
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-11-28 13:50:44 +0530

    Refactor CSS styles and enhance layout consistency across agent and customer views

--- app/views/agent/repairings.view.php
+++ app/views/agent/repairings.view.php
@@ -1,6 +1,7 @@
 <?php require_once 'agentHeader.view.php'; ?>
 
 <div class="user_view-menu-bar">
+    <div class="gap"></div>
     <h2>services</h2>
     <div class="flex-bar">
         <div class="search-container">
