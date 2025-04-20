9bad2fafdc0e0c47bc155660fa7ad5ae38f0c979
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2025-04-20 13:24:17 +0530

    [PRI016E] FEAT : Manage External Maintenances( Haddle Services) view

--- app/views/agent/agentHeader.view.php
+++ app/views/agent/agentHeader.view.php
@@ -59,6 +59,7 @@
                     <li><a href="<?= ROOT ?>/dashboard/preInspection"><button class="btn"><i class="fa-solid fa-file-contract"></i>Pre Inspections</button></a></li>
                     <li><a href="<?= ROOT ?>/dashboard/tasks"><button class="btn"><i class="fa-solid fa-list-check"></i>Tasks</button></a></li>
                     <li><a href="<?= ROOT ?>/dashboard/requestedTasks"><button class="btn"><i class="fa-solid fa-inbox"></i>Requested Tasks</button></a></li>
+                    <li><a href="<?= ROOT ?>/dashboard/externalServiceRequests"><button class="btn" style="text-align: left;"><i class="fa-solid fa-handshake-angle"></i>External service requests</button></a></li>
                     <li><a href="<?= ROOT ?>/dashboard/services"><button class="btn"><i class="fa-solid fa-screwdriver-wrench"></i>Services</button></a></li>
                     <li><a href="<?= ROOT ?>/dashboard/bookings"><button class="btn"><i class="fa-solid fa-house"></i>Bookings</button></a></li>
                     <li><a href="<?= ROOT ?>/dashboard/inventory"><button class="btn"><i class="fa-solid fa-warehouse"></i>Inventory</button></a></li>
