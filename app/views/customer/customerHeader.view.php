d0569cbcedcddc8f7e81d41dece1ba8fa2c59d9f
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2025-04-23 07:45:43 +0530

    [PRI011O] CREATE : Customer Dashboard button

--- app/views/customer/customerHeader.view.php
+++ app/views/customer/customerHeader.view.php
@@ -56,6 +56,7 @@
         <div class="content-section">
             <div class="user_view-sidemenu">
                 <ul>
+                    <li><a href="<?= ROOT ?>/dashboard" data-section="dashboard"><button class="btn"><i class="fa-solid fa-house"></i>Dashboard</button></a></li>
                     <!-- <li><a href="<?= ROOT ?>/dashboard/search/property/propertyLisingToAllUsers" data-section="propertyLisingToAllUsers"><button class="btn"><i class="fa-solid fa-magnifying-glass"></i>Search</button></a></li> -->
                     <li><a href="<?= ROOT ?>/dashboard/payments" data-section="payments"><button class="btn"><i class="fa-solid fa-money-check-dollar"></i>Payments</button></a></li>
                     <li><a href="<?= ROOT ?>/dashboard/occupiedProperties" data-section="occupiedProperties"><button class="btn"><i class="fa-solid fa-house-user"></i>History</button></a></li>
